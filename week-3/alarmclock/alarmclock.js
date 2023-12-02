cb96ed12b139fe7eb57fe53c96be9d583ed99578
Elmira Zangeneh <elmira.zangeneh89@gmail.com>
2023-12-02 16:42:42 +0000

    alarm clock

--- week-3/alarmclock/alarmclock.js
+++ week-3/alarmclock/alarmclock.js
@@ -1,5 +1,37 @@
-function setAlarm() {}
+let time = document.getElementById("timeRemaining").innerHTML;
+let countDown = 0;
+var interval;
 
+function displayTime() {
+  let timeRemainingValue = time.split(":")[0] + ":" + countDown;
+  document.getElementById("timeRemaining").innerHTML = timeRemainingValue;
+}
+
+function setAlarm() {
+  countDown = document.getElementById("alarmTime").value;
+  displayTime();
+  interval = setInterval(function () {
+    if (countDown === 0) {
+      playAlarm();
+      clearInterval();
+    } else {
+      countDown--;
+    }
+    displayTime();
+  }, 1000);
+  // document.getElementById("alarmTime").value = 0;
+  // parseInt(document.querySelector('#'), 10);
+}
+// const pauseAlarm = document.getElementById('pause');
+// console.log(pauseAlarm);
+// pauseAlarm.addEventListener('click', ()=>{
+//   if(pauseAlarm.textContent === 'Pause Alarm'){
+//     clearInterval ()
+//   }
+// })
+const stopAlarm = () => {
+  clearInterval(interval);
+};
 // DO NOT EDIT BELOW HERE
 
 var audio = new Audio("alarmsound.mp3");
@@ -10,6 +42,7 @@ function setup() {
   });
 
   document.getElementById("stop").addEventListener("click", () => {
+    stopAlarm();
     pauseAlarm();
   });
 }
